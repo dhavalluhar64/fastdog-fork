7344ef548514c7b31aff9dc6bc1a61c25fb9d4db
Aaron Crosman <acrosman@gmail.com>
2017-11-04 12:14:31 -0400

    Improved source file scanning to suppress dot files and use full paths

--- parsers/content.js
+++ parsers/content.js
@@ -4,18 +4,20 @@ const yamlFront = require('yaml-front-matter');
 const marked = require('marked');
 
 // Recursive function to walk a directory and return a list of all files with
-// complete path.
+// complete path. Ignores dot files.
 function directoryWalk(directory) {
   const dirList = fs.readdirSync(directory);
   let files = [];
   for (let i = 0; i < dirList.length; i += 1) {
-    const path = [directory, dirList[i]].join('/');
-    if (fs.existsSync(path)) {
-      const f = fs.lstatSync(path);
-      if (f.isFile()) {
-        files.push(path);
-      } else {
-        files = files.concat(directoryWalk(path));
+    if (!dirList[i].startsWith('.')) {
+      const path = [directory, dirList[i]].join('/');
+      if (fs.existsSync(path)) {
+        const f = fs.lstatSync(path);
+        if (f.isFile()) {
+          files.push(path);
+        } else {
+          files = files.concat(directoryWalk(path));
+        }
       }
     }
   }
@@ -24,7 +26,8 @@ function directoryWalk(directory) {
 
 exports.prepareFiles = function prepareFiles(directory, callback) {
   const pages = [];
-  const files = directoryWalk(directory);
+  const realDirectory = fs.realpathSync(directory);
+  const files = directoryWalk(realDirectory);
   let counter = 0;
 
   files.forEach((file) => {
